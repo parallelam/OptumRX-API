70bfe72a890a32c7638a580f971ea0947ad315e9
Wes Hatley <whatley@lenovo.com>
2020-11-22 21:44:22 -0500

    fixing health check for application to pass bp ci/cd

--- src/models/SignUp/SignUpHealthCheck.ts
+++ src/models/SignUp/SignUpHealthCheck.ts
@@ -1,7 +1,5 @@
 import { Request, RequestHandler, Response } from 'express';
 
 export const healthCheck: RequestHandler = (req: Request, res: Response): Response => {
-  console.log('New Healthcheck Request:')
-  console.log(req)
-  return res.status(200);
+  return res.status(200).send('Health Check Performed');
 };
